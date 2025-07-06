f39fb75741c46ae649d66cc189590465c9141e7f
Saad Waseem <s.w150700@hotmail.com>
2025-07-06 12:05:18 +0500

    V2: Created MySQL connector

---
+++ database/connectors/mysql/mysql_connector.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from database.connectors import BaseConnector
+from helpers.env_vars import ENV_VARS
+
+class MysqlConnector(BaseConnector):
+    def __init__(self, host, user, database):
+        super().__init__(host, user, database)
+        self._create_connection(password=ENV_VARS.MYSQL_CONNECTOR_PASSWORD.value)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if hasattr(self, 'connection') and self._connection:
+            self._connection.close()
+
+    def execute_query(self, query, *args, **kwargs):
+        return pd.DataFrame(
+            super().execute_query(query, *args, **kwargs),
+            columns=[col[0] for col in self._cursor.description]
+        )
+
+if __name__ == "__main__":
+    with MysqlConnector('localhost', 'root', 'AGENTIC_TRADER') as db:
+        res = db.execute_query("SELECT * FROM TEST;")
+        print(res)
