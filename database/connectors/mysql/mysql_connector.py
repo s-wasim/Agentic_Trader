679c7562c374bcabf175f04e898179c60da5aaf5
Saad Waseem <s.w150700@hotmail.com>
2025-07-10 23:44:21 +0500

    V2: Improved functionality to support other connectors

--- database/connectors/mysql/mysql_connector.py
+++ database/connectors/mysql/mysql_connector.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy import create_engine
 from database.connectors import BaseConnector
 from helpers.env_vars import ENV_VARS
 
@@ -6,6 +7,7 @@ class MysqlConnector(BaseConnector):
     def __init__(self, host, user, database):
         super().__init__(host, user, database)
         self._create_connection(password=ENV_VARS.MYSQL_CONNECTOR_PASSWORD.value)
+        self._engine = create_engine('mysql+mysqlconnector://', creator=lambda: self._connection)
 
     def __enter__(self):
         return self
@@ -20,6 +22,10 @@ class MysqlConnector(BaseConnector):
             res_set,
             columns=[col[0] for col in self._cursor.description]
         ) if len(res_set) > 0 else None
+
+    @property
+    def engine(self):
+        return self._engine
 
 if __name__ == "__main__":
     with MysqlConnector('localhost', 'root', 'AGENTIC_TRADER') as db:
