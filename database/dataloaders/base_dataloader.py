872b482c02fc3914a9f047e4e9559b62219a32a4
Saad Waseem <s.w150700@hotmail.com>
2025-07-11 23:09:39 +0500

    V2: Added functionalities to base Data Loader

--- database/dataloaders/base_dataloader.py
+++ database/dataloaders/base_dataloader.py
@@ -1,10 +1,32 @@
 from database.connectors.mysql import MysqlConnector
 
+def get_bridge_key(func):
+        def inner(*args, **kwargs):
+            db = kwargs['conn_db']
+            sql = "SELECT {} FROM {} WHERE {};".format(
+                kwargs["select_col"],
+                kwargs["bridge_table_name"],
+                kwargs["where_clause"]
+            )
+            df = db.execute_query(sql)
+            func(fk_id=df['TickerFinancesID'].values[0], *args, **kwargs)
+        return inner
+
 class BaseDataloader:
     def __init__(self, db_type):
         self.db = db_type.value
 
-    def _dump_df_to_db(self, df, db_context, dest_table_name):
+    def _add_str_to_db(self, *args, **kwargs):
+        sql = f"""
+        INSERT INTO {kwargs['table_name']} ({','.join(kwargs['column_names'])})
+        VALUES 
+        ({'),\n('.join(','.join(row) for row in args)});
+        """
+        kwargs['db_context'].execute_query(sql)
+
+    def _dump_df_to_db(self, df, db_context, dest_table_name, full_load=True):
+        if full_load:
+            db_context.execute_query(f"DELETE FROM {dest_table_name}")
         df.to_sql(
             dest_table_name, 
             con=db_context.engine,
